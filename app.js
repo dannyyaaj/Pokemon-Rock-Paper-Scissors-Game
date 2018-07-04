2b4ac7f6c4ef8e92d5d01c8e7c19640726cf1b5c
Danny Yang <dannyyaaj@gmail.com>
2018-07-04 07:15:54 -0500

    changed double quotes to single quotes

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 let userScore = 0;
 let compScore = 0;
-var userScore_span = document.getElementById("user-score");
-var compScore_span = document.getElementById("comp-score");
-const scoreBoard_div = document.querySelector(".score-board");
-var result_div = document.querySelector("p");
-const b_div = document.getElementById("b");
-const c_div = document.getElementById("c");
-const s_div = document.getElementById("s");
-var reset = document.getElementById("reset");
+let userScore_span = document.getElementById('user-score');
+let compScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.querySelector('.score-board');
+let result_div = document.querySelector('p');
+const b_div = document.getElementById('b');
+const c_div = document.getElementById('c');
+const s_div = document.getElementById('s');
+let reset = document.getElementById('reset');
 
 function getComputerChoice() {
-  const choices = ["b", "c", "s"];
+  const choices = ['b', 'c', 's'];
   const randomNum = (Math.floor(Math.random() * 3));
   return choices[randomNum];
 }
 
 function convertToWord(letter) {
-  if (letter === "b") return "Bulbasaur";
-  if (letter === "c") return "Charmander";
-  return "Squirtle";
+  if (letter === 'b') return 'Bulbasaur';
+  if (letter === 'c') return 'Charmander';
+  return 'Squirtle';
 }
 
 function resetUserScore() {
-  userScore_span.innerHTML = "0 ";
+  userScore_span.innerHTML = '0 ';
   return userScore = 0;
 }
 
 function resetCompScore() {
-  compScore_span.innerHTML = " 0";
+  compScore_span.innerHTML = ' 0';
   return compScore = 0;
 }
 
 function win(userChoice, computerChoice) {
   userScore++;
-  userScore_span.innerHTML = userScore + " ";
-  compScore_span.innerHTML = " " + compScore;
-  const smallUserWord = "(you)".fontsize(25);
-  const smallOppWord = "(opponent)".fontsize(25);
+  userScore_span.innerHTML = userScore + ' ';
+  compScore_span.innerHTML = ' ' + compScore;
+  const smallUserWord = '(you)'.fontsize(25);
+  const smallOppWord = '(opponent)'.fontsize(25);
   result_div.innerHTML = `${convertToWord(userChoice)} ${smallUserWord} beats ${convertToWord(computerChoice)} ${smallOppWord}. You win!`;
 }
 
 function lose(userChoice, computerChoice) {
   compScore++;
-  userScore_span.innerHTML = userScore + " ";
-  compScore_span.innerHTML = " " + compScore;
-  const smallUserWord = "(you)".fontsize(25);
-  const smallOppWord = "(opponent)".fontsize(25);
+  userScore_span.innerHTML = userScore + ' ';
+  compScore_span.innerHTML = ' ' + compScore;
+  const smallUserWord = '(you)'.fontsize(25);
+  const smallOppWord = '(opponent)'.fontsize(25);
   result_div.innerHTML = `${convertToWord(userChoice)} ${smallUserWord} loses to ${convertToWord(computerChoice)} ${smallOppWord}. You lose!`;
 }
 
@@ -58,41 +58,41 @@ function draw(userChoice, computerChoice) {
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    case "bs":
-    case "cb":
-    case "sc":
+    case 'bs':
+    case 'cb':
+    case 'sc':
       win(userChoice, computerChoice);
       break;
-    case "sb":
-    case "cs":
-    case "bc":
+    case 'sb':
+    case 'cs':
+    case 'bc':
       lose(userChoice, computerChoice);
       break;
-    case "ss":
-    case "bb":
-    case "cc":
+    case 'ss':
+    case 'bb':
+    case 'cc':
       draw(userChoice, computerChoice);
       break;
   }
 }
-/* if user clicks on buttons (created in HTML as divs), then will run function game ("div user clicked on")*/
+/* if user clicks on buttons (created in HTML as divs), then will run function game ('div user clicked on')*/
 function main() {
 
-  reset.addEventListener("click", function () {
+  reset.addEventListener('click', function () {
     resetUserScore();
     resetCompScore();
   });
 
-  b_div.addEventListener("click", function () {
-    game("b");
+  b_div.addEventListener('click', function () {
+    game('b');
   })
 
-  c_div.addEventListener("click", function () {
-    game("c");
+  c_div.addEventListener('click', function () {
+    game('c');
   })
 
-  s_div.addEventListener("click", function () {
-    game("s");
+  s_div.addEventListener('click', function () {
+    game('s');
   })
 
 }
